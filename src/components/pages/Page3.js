a294ff2504e5f567e8f002bc956a8ec86a3e1148
david <simplesam8856@gmail.com>
2021-10-06 15:04:03 +1100

    google maps

--- src/components/pages/Page3.js
+++ src/components/pages/Page3.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import './Pages.scss'
+
+import background from '../../images/3.jpg'
 
 export default function Page3({ className }) {
   return (
-    <section className={className}>
-      <div className='content-wrapper'>
-        <p className='content-title'>give me your money</p>
-        <p className='content-subtitle'>please, I am desperate!</p>
+    <section
+      className={'page-3 ' + className}
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
+      <div className=' page-3-wrapper '>
+        <iframe
+          title='map'
+          src='https://maps.google.com/maps?q=poacher%20and%20hound&t=&z=15&ie=UTF8&iwloc=&output=embed'
+          width='100%'
+          height='100%'
+          style={{ border: 0 }}
+        />
+
+        <div className=' page-3-contact-wrapper'>
+          <p className='content-title'>Contact Me</p>
+        </div>
       </div>
     </section>
   )
