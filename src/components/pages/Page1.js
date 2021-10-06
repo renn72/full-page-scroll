e7372bf4f1aa41115b1bdbec171f21cb5a4b27ce
david <simplesam8856@gmail.com>
2021-10-06 13:24:42 +1100

    gsap title

--- src/components/pages/Page1.js
+++ src/components/pages/Page1.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import gsap from 'gsap'
+import { Power3 } from 'gsap/gsap-core'
 
 export default function Page1({ className }) {
+  useEffect(() => {
+    gsap.from('.shop-title-1', {
+      opacity: 0,
+      x: 1200,
+      ease: Power3.easeOut,
+      duration: 3.1,
+    })
+    gsap.from('.shop-title-2', {
+      delay: 0.2,
+      opacity: 0,
+      ease: Power3.easeIn,
+      duration: 3.1,
+    })
+    gsap.from('.shop-title-3', {
+      delay: 0.4,
+      opacity: 0,
+      x: -1200,
+      ease: Power3.easeOut,
+      duration: 3.1,
+    })
+  }, [])
+
   return (
     <section className={className}>
       <div className='content-wrapper'>
-        <p className='content-title shop-title'>Poacher</p>
-        <p className='content-title shop-title'>&</p>
-        <p className='content-title shop-title'>Hound</p>
+        <p className='content-title shop-title shop-title-1'>Poacher</p>
+        <p className='content-title shop-title shop-title-2'>&</p>
+        <p className='content-title shop-title shop-title-3'>Hound</p>
         <p className='content-subtitle'>
           Scroll down and up to see the effect!
         </p>
